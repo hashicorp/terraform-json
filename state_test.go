00d6d29724f5f1f91b63d17dae1d21f593996386
Radek Simko <radek.simko@gmail.com>
2020-01-11 11:21:11 +0000

    state: Add UnmarshalJSON to enforce validation

--- state_test.go
+++ state_test.go
@@ -2,11 +2,72 @@ package tfjson
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"os"
 	"testing"
 )
 
-func TestStateValidate(t *testing.T) {
+func TestStateValidate_raw(t *testing.T) {
+	f, err := os.Open("test-fixtures/no_changes/state.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	var state State
+	if err := json.NewDecoder(f).Decode(&state); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := state.Validate(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestStateUnmarshal_valid(t *testing.T) {
+	f, err := os.Open("test-fixtures/no_changes/state.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var state State
+	err = json.Unmarshal(b, &state)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestStateUnmarshal_internalState(t *testing.T) {
+	f, err := os.Open("test-fixtures/no_changes/terraform.tfstate")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var state State
+	err = json.Unmarshal(b, &state)
+	if err == nil {
+		t.Fatal("expected unmarshal to fail")
+	}
+	got := err.Error()
+	expected := "unexpected state input, format version is missing"
+	if expected != got {
+		t.Fatalf("error mismatch.\nexpected: %q\ngot: %q\n", expected, got)
+	}
+}
+
+func TestStateValidate_fromPlan(t *testing.T) {
 	f, err := os.Open("test-fixtures/no_changes/plan.json")
 	if err != nil {
 		t.Fatal(err)
