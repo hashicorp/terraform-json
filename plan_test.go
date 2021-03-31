1e421e47d9d8eee0e26cf47875cb5c0c23163ae1
Radek Simko <radek.simko@gmail.com>
2021-03-31 11:35:44 +0100

    Reflect 0.15 sensitivity fields in plan & config

--- plan_test.go
+++ plan_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"os"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestPlanValidate(t *testing.T) {
@@ -20,5 +22,43 @@ func TestPlanValidate(t *testing.T) {
 
 	if err := plan.Validate(); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestPlan_015(t *testing.T) {
+	f, err := os.Open("testdata/basic/plan-0.15.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	var plan *Plan
+	if err := json.NewDecoder(f).Decode(&plan); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := plan.Validate(); err != nil {
+		t.Fatal(err)
+	}
+
+	expectedChange := &Change{
+		Actions:         Actions{"create"},
+		After:           map[string]interface{}{"ami": "boop"},
+		AfterUnknown:    map[string]interface{}{"id": true},
+		BeforeSensitive: false,
+		AfterSensitive:  map[string]interface{}{"ami": true},
+	}
+	if diff := cmp.Diff(expectedChange, plan.ResourceChanges[0].Change); diff != "" {
+		t.Fatalf("unexpected change: %s", diff)
+	}
+
+	expectedVariable := map[string]*ConfigVariable{
+		"test_var": {
+			Default:   "boop",
+			Sensitive: true,
+		},
+	}
+	if diff := cmp.Diff(expectedVariable, plan.Config.RootModule.Variables); diff != "" {
+		t.Fatalf("unexpected variables: %s", diff)
 	}
 }
