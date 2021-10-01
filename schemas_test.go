60f3688a478ce9bd80b32dfc3742e1a97fd2e967
Radek Simko <radek.simko@gmail.com>
2021-10-01 10:10:56 +0100

    add panicking test

--- schemas_test.go
+++ schemas_test.go
@@ -22,3 +22,20 @@ func TestProviderSchemasValidate(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestProviderSchemasValidate_nestedAttributes(t *testing.T) {
+	f, err := os.Open("testdata/nested_attributes/schemas.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	var schemas *ProviderSchemas
+	if err := json.NewDecoder(f).Decode(&schemas); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := schemas.Validate(); err != nil {
+		t.Fatal(err)
+	}
+}
