d0fd3e9caceab950f6444a0a544d1453d4c4e539
James Humphries <james@yantr.io>
2022-07-01 11:31:36 +0100

    Added test cases for sanitize_plan

--- sanitize/sanitize_plan_variables_test.go
+++ sanitize/sanitize_plan_variables_test.go
@@ -1,6 +1,7 @@
 package sanitize
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -46,6 +47,38 @@ func variablesCases() []testVariablesCase {
 	}
 }
 
+func dynamicVariableCases() []testVariablesCase {
+	return []testVariablesCase{
+		{
+			name: "basic",
+			old: map[string]*tfjson.PlanVariable{
+				"foo": &tfjson.PlanVariable{
+					Value: "test-foo",
+				},
+				"bar": &tfjson.PlanVariable{
+					Value: "test-bar",
+				},
+			},
+			configs: map[string]*tfjson.ConfigVariable{
+				"foo": &tfjson.ConfigVariable{
+					Sensitive: false,
+				},
+				"bar": &tfjson.ConfigVariable{
+					Sensitive: true,
+				},
+			},
+			expected: map[string]*tfjson.PlanVariable{
+				"foo": &tfjson.PlanVariable{
+					Value: "test-foo",
+				},
+				"bar": &tfjson.PlanVariable{
+					Value: "TEST-BAR",
+				},
+			},
+		},
+	}
+}
+
 func TestSanitizePlanVariables(t *testing.T) {
 	for i, tc := range variablesCases() {
 		tc := tc
@@ -61,6 +94,32 @@ func TestSanitizePlanVariables(t *testing.T) {
 
 			if diff := cmp.Diff(variablesCases()[i].old, tc.old); diff != "" {
 				t.Errorf("SanitizePlanVariables() altered original (-expected +actual):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestSanitizePlanVariablesDynamic(t *testing.T) {
+	for i, tc := range dynamicVariableCases() {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := SanitizePlanVariablesDynamic(tc.old, tc.configs, func(old interface{}) interface{} {
+				// if the old value is a string, call ToUpper, else return the default redacted value
+				if s, ok := old.(string); ok {
+					return strings.ToUpper(s)
+				}
+				return DefaultSensitiveValue
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(tc.expected, actual); diff != "" {
+				t.Errorf("SanitizePlanVariablesDynamic() mismatch (-expected +actual):\n%s", diff)
+			}
+
+			if diff := cmp.Diff(dynamicVariableCases()[i].old, tc.old); diff != "" {
+				t.Errorf("SanitizePlanVariablesDynamic() altered original (-expected +actual):\n%s", diff)
 			}
 		})
 	}
