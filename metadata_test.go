3f28642cabb08ba19ccd04e37da8e4683fd1bb97
Daniel Banck <daniel@dbanck.de>
2023-02-02 17:52:49 +0100

    Test metadata functions validate

---
+++ metadata_test.go
@@ -0,0 +1,24 @@
+package tfjson
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+)
+
+func TestMetadataFunctionsValidate(t *testing.T) {
+	f, err := os.Open("testdata/basic/functions.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	var functions *MetadataFunctions
+	if err := json.NewDecoder(f).Decode(&functions); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := functions.Validate(); err != nil {
+		t.Fatal(err)
+	}
+}
