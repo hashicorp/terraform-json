338fe142c8b929082e7af28d7f2c97e802cc79ac
Sarah French <15078782+SarahFrench@users.noreply.github.com>
2024-12-12 11:18:09 +0000

    Add support for write-only attributes (#146)

--- schemas_test.go
+++ schemas_test.go
@@ -10,58 +10,47 @@ import (
 )
 
 func TestProviderSchemasValidate(t *testing.T) {
-	f, err := os.Open("testdata/basic/schemas.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
-
-	var schemas *ProviderSchemas
-	if err := json.NewDecoder(f).Decode(&schemas); err != nil {
-		t.Fatal(err)
-	}
-
-	if err := schemas.Validate(); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestProviderSchemasValidate_functions(t *testing.T) {
-	f, err := os.Open("testdata/functions/schemas.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
-
-	var schemas *ProviderSchemas
-	if err := json.NewDecoder(f).Decode(&schemas); err != nil {
-		t.Fatal(err)
+	cases := map[string]struct {
+		testDataPath string
+	}{
+		"a basic provider schema is validated": {
+			testDataPath: "testdata/basic/schemas.json",
+		},
+		"a provider schema including functions is validated": {
+			testDataPath: "testdata/functions/schemas.json",
+		},
+		"a provider schema including ephemeral resources is validated": {
+			testDataPath: "testdata/ephemeral_resources/schemas.json",
+		},
+		"a provider schema including a resource with write-only attribute(s) is validated": {
+			testDataPath: "testdata/write_only_attribute_on_resource/schemas.json",
+		},
 	}
 
-	if err := schemas.Validate(); err != nil {
-		t.Fatal(err)
-	}
-}
+	for tn, tc := range cases {
+		t.Run(tn, func(t *testing.T) {
+			f, err := os.Open(tc.testDataPath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer f.Close()
 
-func TestProviderSchemasValidate_ephemeralResources(t *testing.T) {
-	f, err := os.Open("testdata/ephemeral_resources/schemas.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
+			var schemas *ProviderSchemas
+			if err := json.NewDecoder(f).Decode(&schemas); err != nil {
+				t.Fatal(err)
+			}
 
-	var schemas *ProviderSchemas
-	if err := json.NewDecoder(f).Decode(&schemas); err != nil {
-		t.Fatal(err)
-	}
-
-	if err := schemas.Validate(); err != nil {
-		t.Fatal(err)
+			if err := schemas.Validate(); err != nil {
+				t.Fatal(err)
+			}
+		})
 	}
 }
 
-func TestProviderSchemasValidate_nestedAttributes(t *testing.T) {
-	f, err := os.Open("testdata/nested_attributes/schemas.json")
+// TestProviderSchemas_writeOnlyAttribute asserts that write-only attributes in a resource in a
+// provider schema JSON file are marked as WriteOnly once decoded into a ProviderSchemas struct
+func TestProviderSchemas_writeOnlyAttribute(t *testing.T) {
+	f, err := os.Open("testdata/write_only_attribute_on_resource/schemas.json")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -72,7 +61,11 @@ func TestProviderSchemasValidate_nestedAttributes(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if err := schemas.Validate(); err != nil {
-		t.Fatal(err)
+	resourceSchema := schemas.Schemas["terraform.io/builtin/terraform"].ResourceSchemas["terraform_example"]
+	if resourceSchema.Block.Attributes["wo_attr"].WriteOnly != true {
+		t.Fatal("expected terraform_example.wo_attr to be marked as write-only")
+	}
+	if resourceSchema.Block.Attributes["foo"].WriteOnly != false {
+		t.Fatal("expected terraform_example.foo to not be marked as write-only")
 	}
 }
